98984a39d222057691117f6806627aefbf733ba9
dv970406 <dv970406@gmail.com>
2023-08-08 13:21:35 +0900

    feat: Add msw 환경 세팅
    - 백엔드 데이터 테스트를 위한 msw환경 구축

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,4 +2,15 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+import { server } from "./mocks/server";
+
+// 모든 테스트 이전에 서버를 시작
+beforeAll(() => server.listen());
+
+// 각 테스트가 끝날 때마다 handlers 초기화
+afterEach(() => server.resetHandlers());
+
+// 모든 테스트 이후에 서버 종료
+afterAll(() => server.close());
