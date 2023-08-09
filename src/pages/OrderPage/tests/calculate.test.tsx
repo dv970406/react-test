9a78d04f7b045726c89714f925b02612eeca9bda
dv970406 <dv970406@gmail.com>
2023-08-09 15:30:34 +0900

    test: Add 상품가격, 옵션가격을 더한 총 가격 구하기
    - 1. total price의 초기값과 product 개수가 변했을 때의 total price 값 계산
    - 2. option 추가 시 total price가 업데이트 되는가
    - 3. option과 product 개수 감소 시 total price가 업데이트 되는가

--- src/pages/OrderPage/tests/calculate.test.tsx
+++ src/pages/OrderPage/tests/calculate.test.tsx
@@ -1,6 +1,7 @@
 import Type from "../Type";
 import userEvent from "@testing-library/user-event";
 import { render, screen } from "../../../test.utils";
+import OrderPage from "..";
 
 test("update product's total when products change", async () => {
   render(<Type orderType="products" />);
@@ -43,4 +44,61 @@ test("update option's total when options change", async () => {
   // 디너 옵션 제거
   userEvent.click(dinnerCheckbox);
   expect(optionsTotal).toHaveTextContent("500");
+});
+
+describe("total price of products and options", () => {
+  // total price가 0부터 시작해서 product를 추가했을 때 total price가 제대로 변화되는가
+  test("total price starts with 0 and updating total price when adding one product", async () => {
+    render(<OrderPage />);
+
+    const total = screen.getByText("Total Price:", { exact: false });
+    expect(total).toHaveTextContent("0");
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    expect(total).toHaveTextContent("1000");
+  });
+
+  // option 추가 시 total price가 업데이트 되는가
+  test("updating total price when adding one option", async () => {
+    render(<OrderPage />);
+    const total = screen.getByText("Total Price:", { exact: false });
+
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+    userEvent.click(insuranceCheckbox);
+    expect(total).toHaveTextContent("500");
+  });
+
+  // option와 product 제거 시 total price가 업데이트 되는가
+  test("updating total price when removing option and product", async () => {
+    render(<OrderPage />);
+    const total = screen.getByText("Total Price:", { exact: false });
+
+    // insurance : 500원
+    const insuranceCheckbox = await screen.findByRole("checkbox", {
+      name: "Insurance",
+    });
+    userEvent.click(insuranceCheckbox);
+
+    const americaInput = await screen.findByRole("spinbutton", {
+      name: "America",
+    });
+
+    // america product 3개: 3000 + insurance: 500 = 3500
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "3");
+
+    // america product를 다시 1개로 수정하면?
+    userEvent.clear(americaInput);
+    userEvent.type(americaInput, "1");
+
+    // america product 1개: 1000 + insurance: 500 = 1500
+    expect(total).toHaveTextContent("1500");
+  });
 });
